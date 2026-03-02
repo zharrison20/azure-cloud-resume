c86322fa4afd608d243e9526e1b5e1a30ab986a0
zharrison20 <zharrison20@gmail.com>
2026-03-02 15:35:18 -0800

    updated frontend for counter, tested backend locally

--- webpage/assets/js/counter.js
+++ webpage/assets/js/counter.js
@@ -1,4 +1,18 @@
-const apiLink = 'counter-bfgvbeanc7fvdrhk.westus2-01.azurewebsites.net';
-fetch(apiLink)
-  .then(res => res.json())
-  .then(data => document.getElementById('visitorCount').innerText = data.count);
+window.addEventListener('DOMContentLoaded', (event) =>{
+  getVistitCount();
+})
+
+const functionApi = 'counter-bfgvbeanc7fvdrhk.westus2-01.azurewebsites.net'
+
+const getVistitCount = () => {
+  let count = 30;
+  fetch(functionApi).then(response => {
+    return response.json()
+  }).then(response =>{
+    console.log("Website called function API");
+    count = response.count;
+    document.getElementById("counter").innerText = count;
+  }).catch(function(error){
+    console.log(error);
+  })
+}
